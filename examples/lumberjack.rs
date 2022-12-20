bf863720319c068dfbae961b77152f22dc76c661
dmackdev <dmackdev@gmail.com>
2022-12-20 16:34:19 +0000

    Improved lumberjack example.

--- examples/lumberjack.rs
+++ examples/lumberjack.rs
@@ -19,7 +19,7 @@ fn create_lumberjack(mut commands: Commands) {
         .with_precondition(ActorHasAxeCondition, false)
         .with_postcondition(ActorHasAxeCondition, true);
 
-    let chop_tree_action = Action::build(ChopTreeAction)
+    let chop_tree_action = Action::build(ChopTreeAction::new(5))
         .with_precondition(ActorHasAxeCondition, true)
         .with_postcondition(ActorHasWoodCondition, true);
 
@@ -27,14 +27,14 @@ fn create_lumberjack(mut commands: Commands) {
         .with_precondition(ActorHasWoodCondition, false)
         .with_postcondition(ActorHasWoodCondition, true);
 
-    // Try toggling the initial conditions to observe the different actions the lumberjack takes!
+    // Try toggling the initial conditions and removing actions to observe the different action sequences the lumberjack performs!
     let lumberjack = Actor::build(Lumberjack)
         .with_initial_condition(ActorHasAxeCondition, false)
         .with_initial_condition(ActorHasWoodCondition, false)
         .with_goal(ActorHasWoodCondition, true)
         .with_action(get_axe_action)
-        .with_action(chop_tree_action)
-        .with_action(collect_wood_action);
+        .with_action(collect_wood_action)
+        .with_action(chop_tree_action);
 
     commands.spawn_empty().insert(lumberjack);
 }
@@ -65,20 +65,29 @@ struct ActorHasAxeCondition;
 impl Condition for ActorHasAxeCondition {}
 
 #[derive(Component, Clone)]
-struct ChopTreeAction;
+struct ChopTreeAction {
+    max_chops: u8,
+    current_chops: u8,
+}
 
-fn chop_tree_action_system(mut query: Query<&mut ActionState, With<ChopTreeAction>>) {
-    for mut action_state in query.iter_mut() {
-        match *action_state {
-            ActionState::Executing => {
-                println!("Chopping tree!");
+impl ChopTreeAction {
+    fn new(max_chops: u8) -> Self {
+        Self {
+            max_chops,
+            current_chops: 0,
+        }
+    }
+}
 
+fn chop_tree_action_system(mut query: Query<(&mut ActionState, &mut ChopTreeAction)>) {
+    for (mut action_state, mut chop_tree_action) in query.iter_mut() {
+        if let ActionState::Executing = *action_state {
+            chop_tree_action.current_chops += 1;
+            println!("Chopped tree {} times!", chop_tree_action.current_chops);
+
+            if chop_tree_action.current_chops >= chop_tree_action.max_chops {
                 *action_state = ActionState::Complete;
             }
-            ActionState::Complete => {
-                println!("MoveToTreeAction is Complete.");
-            }
-            _ => {}
         };
     }
 }
@@ -88,16 +97,10 @@ struct CollectWoodAction;
 
 fn collect_wood_action_system(mut query: Query<&mut ActionState, With<CollectWoodAction>>) {
     for mut action_state in query.iter_mut() {
-        match *action_state {
-            ActionState::Executing => {
-                println!("Collecting wood!");
+        if let ActionState::Executing = *action_state {
+            println!("Collecting wood!");
 
-                *action_state = ActionState::Complete;
-            }
-            ActionState::Complete => {
-                println!("CollectWoodAction is Complete.");
-            }
-            _ => {}
+            *action_state = ActionState::Complete;
         };
     }
 }
